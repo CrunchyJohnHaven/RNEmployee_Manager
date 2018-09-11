925e21453401dacc8e874a991ac779887d6f5848
John Bradley <johnhavenbradley@sappercoding.com>
2018-09-11 10:27:29 -0700

    employee list

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import ReduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'; 
+import firebase from 'firebase';
+import reducers from './reducers';
+import Router from './Router';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component { 
+  componentWillMount() {
+    const config = {
+    apiKey: "AIzaSyBJiTJl5hvNRZpYlpd0fX1e4T4zCe58Vbk",
+    authDomain: "manager-53083.firebaseapp.com",
+    databaseURL: "https://manager-53083.firebaseio.com",
+    projectId: "manager-53083",
+    storageBucket: "manager-53083.appspot.com",
+    messagingSenderId: "375281834658"
+  };
+  firebase.initializeApp(config);
+  }
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    <Provider store={store}>
+      <Router />
+    </Provider> 
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
